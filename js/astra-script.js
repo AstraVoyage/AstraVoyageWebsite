78aa22c704da2786e3ef8dbfbd40d50de71b78f1
Daniel Mena <slowpoke@Daniels-Air.lan>
2022-06-08 19:28:20 -0700

    updated css

--- js/astra-script.js
+++ js/astra-script.js
@@ -17,12 +17,16 @@ for(var i = 0; i < 200; i++) {
   star.style.left = x + "%";
   star.style.animationDuration = s + "s";
 
-  var sky = document.getElementById("astra-voyage-header");
-  sky.appendChild(star);
+  //create new element and add to html
+  var star1 = document.createElement("div");
+  star1.classList.add("star");
 
-  star.style.width = d + "px";
-  star.style.height = d + "px";
-  star.style.top = y + "%";
-  star.style.left = x + "%";
-  star.style.animationDuration = s + "s";
+  var sky1 = document.getElementById("astra-voyage-header");
+  sky1.appendChild(star1);
+
+  star1.style.width = d + "px";
+  star1.style.height = d + "px";
+  star1.style.top = y + "%";
+  star1.style.left = x + "%";
+  star1.style.animationDuration = s + "s";
 }
